c9240b1a46de266a445ca7a3648162f591727cb6
york8536 <york8536@gmail.com>
2024-03-04 20:01:26 +0800

    20240304
    新增動畫/音效
    新增Replay按鈕

--- Assets/Scripts/Palyer.cs
+++ Assets/Scripts/Palyer.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     GameObject currentFloor; //建立一個GameObject變數
-    [SerializeField] int Hp;
-    [SerializeField] GameObject HpBar;
-    [SerializeField] TMP_Text scoreText ; 
+    [SerializeField] int Hp; //建立血量
+    [SerializeField] GameObject HpBar; //建立血條框
+    [SerializeField] TMP_Text scoreText ; //建立樓層文字UI
 
     int score;
     float scoreTime;
+    Animator anim;
+    SpriteRenderer render;
+    AudioSource deathSound;
+    [SerializeField] GameObject replayButton;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +26,11 @@ public class player : MonoBehaviour
         Hp = 10;
         score = 0;
         scoreTime = 0f;
-
+        render = GetComponent<SpriteRenderer>();
+        // 在Unity中的Animation點擊對應節點後可建立動畫
+        // 在Unity中的Animator可建立動畫觸發條件與調整流程,建立後可在腳本中控制;
+        anim = GetComponent<Animator>();
+        deathSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,16 +39,24 @@ public class player : MonoBehaviour
         // 左右移動製作
         if(Input.GetKey(KeyCode.RightArrow)){
             transform.Translate(moveSpeed*Time.deltaTime, 0, 0);
+            render.flipX = false;
+            anim.SetBool("run", true);
         }
         else if(Input.GetKey(KeyCode.LeftArrow)){
             transform.Translate(-moveSpeed*Time.deltaTime, 0, 0);
+            render.flipX = true;
+            anim.SetBool("run", true);
+            
+        }
+        else{
+            anim.SetBool("run", false); // 若不動時關閉run動畫
         }
         UpdateScore();
        
     }
 
     
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D other) { // 角色碰撞事件
         // 如果踩到一般地板
         if (other.gameObject.tag == "Normal"){
             // 如果碰撞到底板的上邊
@@ -48,6 +65,7 @@ public class player : MonoBehaviour
                 currentFloor = other.gameObject;
                 // 回1滴血
                 ModifyHp(1);
+                other.gameObject.GetComponent<AudioSource>().Play();
             }
         }
         // 如果踩到有刺地板
@@ -55,8 +73,9 @@ public class player : MonoBehaviour
             if(other.contacts[0].normal == new Vector2(0f, 1f)){
                 Debug.Log("Nails");
                 currentFloor = other.gameObject;
-                // 扣三滴血
-                ModifyHp(-3);
+                ModifyHp(-3); // 扣三滴血
+                anim.SetTrigger("hurt"); // 觸發hurt動畫
+                other.gameObject.GetComponent<AudioSource>().Play();
             }
         }
 
@@ -64,29 +83,33 @@ public class player : MonoBehaviour
         else if (other.gameObject.tag == "Ceiling"){
             Debug.Log("Ceiling");
             currentFloor.GetComponent<BoxCollider2D>().enabled = false;
-            ModifyHp(-3);
+            ModifyHp(-3); // 扣三滴血
+            anim.SetTrigger("hurt"); // 觸發hurt動畫
+            other.gameObject.GetComponent<AudioSource>().Play();
         }
         
     }
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) { // 角色掉落事件
         if (other.gameObject.tag == "die"){
             Debug.Log("die");
+            Die();
+            
         }
     }
 
-    void ModifyHp(int num){
+    void ModifyHp(int num){ // 更新當前血量
         Hp += num;
         if(Hp > 10){
             Hp = 10;
         }
         else if(Hp < 0){
             Hp = 0;
+            Die();
         }
         UpdateHpBar();
     }
 
-    void UpdateHpBar()
-    {
+    void UpdateHpBar(){ // 更新當前HpBar
         for(int i=0; i<HpBar.transform.childCount; i++){
             if(Hp>i){
                 HpBar.transform.GetChild(i).gameObject.SetActive(true);
@@ -98,13 +121,23 @@ public class player : MonoBehaviour
        
     }
 
-    void UpdateScore(){
+    void UpdateScore(){  // 更新闖關分數
         scoreTime += Time.deltaTime;
         if(scoreTime>2f){
             score++;
             scoreTime = 0f;
             scoreText.text = "地下" + score.ToString() + "層";
         }
+    }
+    void Die(){ // 死亡觸發事件
+        deathSound.Play(); // 撥放死亡音效
+        Time.timeScale = 0f; // 遊戲暫停
+        replayButton.SetActive(true);
+    }
+
+    public void Replay(){ // Replay按鍵功能
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SampleScene");
     }
 
 }
